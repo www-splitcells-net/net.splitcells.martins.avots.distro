320bffba3693e4f413e54b04df238364e7c9d26d
Mārtiņš Avots <martins.avots@splitcells.net>
2024-02-16 23:53:27 +0100

    #170 Make gaming running again
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/martins/avots/distro/DevDistro.java
+++ src/main/java/net/splitcells/martins/avots/distro/DevDistro.java
@@ -19,10 +19,17 @@ import net.splitcells.cin.CinFileSystem;
 import net.splitcells.dem.Dem;
 import net.splitcells.dem.DemApiFileSystem;
 import net.splitcells.dem.DemFileSystem;
+import net.splitcells.dem.data.set.list.List;
 import net.splitcells.dem.environment.Environment;
+import net.splitcells.dem.object.Discoverable;
+import net.splitcells.dem.resource.ContentType;
 import net.splitcells.gel.GelCoreFileSystem;
+import net.splitcells.gel.data.database.Databases;
+import net.splitcells.gel.data.table.Table;
 import net.splitcells.gel.doc.GelDocFileSystem;
 import net.splitcells.gel.ext.GelExtFileSystem;
+import net.splitcells.gel.solution.SolutionView;
+import net.splitcells.gel.solution.Solutions;
 import net.splitcells.gel.ui.GelUiFileSystem;
 import net.splitcells.network.NetworkFileSystem;
 import net.splitcells.network.blog.NetworkBlogFileSystem;
@@ -37,12 +44,33 @@ import net.splitcells.system.SystemsFileSystem;
 import net.splitcells.website.WebsiteServerFileSystem;
 import net.splitcells.website.binaries.BinaryFileSystem;
 import net.splitcells.website.content.defaults.WebsiteContentDefaultsFileSystem;
+import net.splitcells.website.server.Config;
+import net.splitcells.website.server.processor.BinaryMessage;
+import net.splitcells.website.server.project.ProjectRenderer;
+import net.splitcells.website.server.project.renderer.DiscoverableMediaRenderer;
+import net.splitcells.website.server.project.renderer.DiscoverableRenderer;
+import net.splitcells.website.server.project.renderer.ObjectsMediaRenderer;
+import net.splitcells.website.server.project.renderer.ObjectsRenderer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
+import static net.splitcells.cin.World.WORLD_HISTORY;
+import static net.splitcells.cin.World.addNextTime;
+import static net.splitcells.cin.World.allocateBlinker;
+import static net.splitcells.cin.World.allocateRestAsDead;
+import static net.splitcells.cin.World.initWorldHistory;
+import static net.splitcells.cin.World.worldHistory;
+import static net.splitcells.cin.World.worldOptimizer;
+import static net.splitcells.dem.data.set.list.Lists.list;
 import static net.splitcells.dem.resource.FileSystemUnion.fileSystemsUnion;
 import static net.splitcells.dem.resource.FileSystems.fileSystemOnLocalHost;
+import static net.splitcells.dem.resource.communication.log.LogLevel.INFO;
+import static net.splitcells.dem.utils.Time.reportRuntime;
+import static net.splitcells.gel.solution.optimization.primitive.OnlineLinearInitialization.onlineLinearInitialization;
+import static net.splitcells.sep.Network.network;
+import static net.splitcells.website.server.processor.BinaryMessage.binaryMessage;
 
 public class DevDistro {
 
@@ -52,7 +80,31 @@ public class DevDistro {
     private static final Path PUBLIC_ROOT_SUB_PROJECTS = PUBLIC_ROOT_PROJECT_REPO.resolve("projects");
 
     public static void main(String... args) {
+        System.setProperty(net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY_KEY, "false");
         Dem.process(() -> {
+            if (true) {
+                final var network = network();
+                final var currentWorldHistory = worldHistory(WORLD_HISTORY, list(), list());
+                reportRuntime(() -> {
+                    network.withNode(WORLD_HISTORY, currentWorldHistory);
+                    initWorldHistory(currentWorldHistory);
+                    allocateBlinker(currentWorldHistory);
+                    currentWorldHistory.init();
+                    allocateRestAsDead(currentWorldHistory);
+                }, "Initialize world history.", INFO);
+                reportRuntime(() -> {
+                    network.withOptimization(WORLD_HISTORY, onlineLinearInitialization());
+                    network.withOptimization(WORLD_HISTORY, worldOptimizer(network.node(WORLD_HISTORY)));
+                }, "Initial world history optimization", INFO);
+                reportRuntime(() -> {
+                    network.withExecution(WORLD_HISTORY, wh -> {
+                        addNextTime(wh);
+                        wh.init();
+                    });
+                    network.withOptimization(WORLD_HISTORY, onlineLinearInitialization());
+                    network.withOptimization(WORLD_HISTORY, worldOptimizer(network.node(WORLD_HISTORY)));
+                }, "World history optimization", INFO);
+            }
             try (final var liveService = Distro.liveService()) {
                 liveService.start();
                 Dem.waitIndefinitely();
@@ -60,6 +112,70 @@ public class DevDistro {
         }, env -> {
             Distro.envConfig(env);
             useLocalFileSystem(env);
+            env.config().configValue(Databases.class)
+                    .withConnector(database -> ObjectsRenderer.registerObject(new DiscoverableRenderer() {
+                        @Override
+                        public String render() {
+                            return database.toHtmlTable().toHtmlString();
+                        }
+
+                        @Override
+                        public Optional<String> title() {
+                            return Optional.of(database.path().toString());
+                        }
+
+                        @Override
+                        public List<String> path() {
+                            return database.path();
+                        }
+                    }));
+            env.config().configValue(Solutions.class)
+                    .withConnector(solution -> ObjectsRenderer.registerObject(new DiscoverableRenderer() {
+                        @Override
+                        public String render() {
+                            return solution.toHtmlTable().toHtmlString();
+                        }
+
+                        @Override
+                        public Optional<String> title() {
+                            return Optional.of(solution.path().toString());
+                        }
+
+                        @Override
+                        public List<String> path() {
+                            return solution.path();
+                        }
+                    }));
+            env.config().configValue(Solutions.class)
+                    .withConnector(solution -> ObjectsRenderer.registerObject(new DiscoverableRenderer() {
+                        @Override
+                        public String render() {
+                            return solution.constraint().renderToHtml().toHtmlString();
+                        }
+
+                        @Override
+                        public Optional<String> title() {
+                            return Optional.of(solution.path().toString());
+                        }
+
+                        @Override
+                        public List<String> path() {
+                            return solution.path().withAppended("constraint");
+                        }
+                    }));
+            env.config().configValue(Solutions.class)
+                    .withConnector(solution -> ObjectsMediaRenderer.registerMediaObject(new DiscoverableMediaRenderer() {
+                        @Override
+                        public Optional<BinaryMessage> render(ProjectRenderer projectRenderer, Config config) {
+                            return Optional.of(binaryMessage(solution.toCSV().getBytes(), ContentType.CSV.codeName()));
+                        }
+
+                        @Override
+                        public List<String> path() {
+                            final var path = solution.path().shallowCopy();
+                            return path.withAppended(path.removeAt(path.size() - 1) + ".csv");
+                        }
+                    }));
         });
     }
 
