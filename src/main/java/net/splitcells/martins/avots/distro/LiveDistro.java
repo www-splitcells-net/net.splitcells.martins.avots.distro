1c9128829237024b81a2d914568ed282d2d82449
Mārtiņš Avots <martins.avots@splitcells.net>
2024-04-22 19:26:29 +0200

    #c11 Add task
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/martins/avots/distro/LiveDistro.java
+++ src/main/java/net/splitcells/martins/avots/distro/LiveDistro.java
@@ -28,6 +28,7 @@ public class LiveDistro {
         Dem.process(() -> {
             try (final var liveService = Distro.liveService()) {
                 liveService.start();
+                // TODO Is this sleep really needed?
                 sleepAtLeast(3000l);
                 try {
                     certificate("live.splitcells.net", "contacts@splitcells.net");
