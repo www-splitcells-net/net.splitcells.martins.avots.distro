3f13c11e70662c0c959657f22526222bd2e0b59e
Mārtiņš Avots <martins.avots@splitcells.net>
2024-11-15 22:45:09 +0100

    Enable runtime checks for easier development #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/martins/avots/distro/DevDistro.java
+++ src/main/java/net/splitcells/martins/avots/distro/DevDistro.java
@@ -63,7 +63,7 @@ public class DevDistro {
     private static final Path PUBLIC_ROOT_SUB_PROJECTS = PUBLIC_ROOT_PROJECT_REPO.resolve("projects");
 
     public static void main(String... args) {
-        System.setProperty(net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY_KEY, "false");
+        System.setProperty(net.splitcells.dem.environment.config.StaticFlags.ENFORCING_UNIT_CONSISTENCY_KEY, "true");
         Dem.process(() -> {
             try (final var liveService = Distro.liveService()) {
                 liveService.start();
