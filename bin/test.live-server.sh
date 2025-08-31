970c21b50cf37efafbfd3f942995e8b53ba6bfc8
Mārtiņš Avots <martins.avots@splitcells.net>
2025-08-31 16:19:12 +0200

    Create live server test command #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ bin/test.live-server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+set -e
+[ ! -d "bin" ] && cd ..
+cd ../net.splitcells.network
+bin/worker.execute.py \
+  --bootstrap-remote=martins-avots@live.splitcells.net \
+  --auto-configure-cpu-architecture-explicitly=true \
+  --verbose=true
+bin/worker.execute.py \
+  --build-remote=martins-avots@live.splitcells.net \
+  --auto-configure-cpu-architecture-explicitly=true \
+  --verbose=true
+bin/worker.execute.py \
+  --execute-via-ssh-at=martins-avots@live.splitcells.net \
+  --command="cd ~/.local/state/net.splitcells.network.worker/repos/public/net.splitcells.network && bin/test.everything" \
+  --auto-configure-cpu-architecture-explicitly=true
