bc5a34346d8661f310516b8b0bf8ab6d9f6c5c00
Mārtiņš Avots <martins.avots@splitcells.net>
2025-09-20 20:25:11 +0200

    Mark worker.test.at as ready #30
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/test.live-server.sh
+++ bin/test.live-server.sh
@@ -4,15 +4,4 @@
 set -e
 [ ! -d "bin" ] && cd ..
 cd ../net.splitcells.network
-bin/worker.execute.py \
-  --bootstrap-remote=martins-avots@live.splitcells.net \
-  --auto-configure-cpu-architecture-explicitly=true \
-  --verbose=true
-bin/worker.execute.py \
-  --build-remote=martins-avots@live.splitcells.net \
-  --auto-configure-cpu-architecture-explicitly=true \
-  --verbose=true
-bin/worker.execute.py \
-  --execute-via-ssh-at=martins-avots@live.splitcells.net \
-  --command="cd ~/.local/state/net.splitcells.network.worker/repos/public/net.splitcells.network && bin/repos.test.sh" \
-  --auto-configure-cpu-architecture-explicitly=true > target/del.log
+bin/worker.test.at martins-avots@live.splitcells.net
