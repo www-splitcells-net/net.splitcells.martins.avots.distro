c611ab916e2996b22e6e3a62bab2b72336615957
Mārtiņš Avots <martins.avots@splitcells.net>
2025-10-26 16:10:55 +0100

    Move LiveDistro to Cell, #42
    as a prepration for migration.
    
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/martins/avots/distro/LiveDistroCell.java
+++ src/main/java/net/splitcells/martins/avots/distro/LiveDistroCell.java
@@ -18,13 +18,108 @@ package net.splitcells.martins.avots.distro;
 import net.splitcells.dem.Dem;
 import net.splitcells.dem.environment.Cell;
 import net.splitcells.dem.environment.Environment;
+import net.splitcells.dem.environment.resource.Console;
+import net.splitcells.dem.resource.communication.log.Logs;
+import net.splitcells.dem.resource.communication.log.MessageFilter;
 import net.splitcells.gel.ext.GelExtCell;
 import net.splitcells.network.distro.DistroCell;
+import net.splitcells.network.distro.java.acme.AcmeServerUri;
+import net.splitcells.network.distro.java.acme.PublicKeyCryptoConfig;
 import net.splitcells.network.system.SystemCell;
+import net.splitcells.website.server.RedirectServer;
 import net.splitcells.website.server.ServerConfig;
+import net.splitcells.website.server.config.InternalPublicPort;
+import net.splitcells.website.server.config.PasswordAuthenticationEnabled;
+import net.splitcells.website.server.config.PublicContactEMailAddress;
+import net.splitcells.website.server.config.PublicDomain;
+import net.splitcells.website.server.security.authentication.Authentication;
+import net.splitcells.website.server.security.authorization.Authorization;
+import net.splitcells.website.server.security.encryption.PrivateIdentityPemStore;
+import net.splitcells.website.server.security.encryption.PublicIdentityPemStore;
+import net.splitcells.website.server.security.encryption.SslEnabled;
+import net.splitcells.website.server.test.HtmlLiveTest;
+import net.splitcells.website.server.test.HtmlLiveTester;
+
+import java.util.Optional;
+
+import static net.splitcells.dem.resource.communication.log.LogLevel.DEBUG;
+import static net.splitcells.dem.resource.communication.log.ServerLogger.serverLog;
+import static net.splitcells.gel.ui.editor.geal.EditorProcessorTest.TEST_OPTIMIZATION_GUI;
+import static net.splitcells.network.distro.java.DistroCell.ensureSslCertificatePresence;
+import static net.splitcells.network.distro.java.DistroCell.setGlobalUnixStateLogger;
+import static net.splitcells.network.distro.java.acme.AcmeServerUri.PRODUCTION_ACME_SERVER;
+import static net.splitcells.network.distro.java.acme.PublicKeyCryptoConfigurator.publicKeyCryptoConfig;
+import static net.splitcells.network.distro.java.acme.SelfSignedPublicKeyCryptoConfigurator.selfSignedPublicKeyCryptoConfigurator;
+import static net.splitcells.website.server.security.authentication.AuthenticatorImpl.authenticatorBasedOnFiles;
+import static net.splitcells.website.server.security.authorization.AuthorizerBasedOnFiles.authorizerBasedOnFiles;
 
 public class LiveDistroCell implements Cell {
+
     public static void main(String... args) {
+        Dem.process(() -> {
+            final PublicKeyCryptoConfig certificate;
+            try (final var liveService = net.splitcells.martins.avots.distro.DistroCell.liveService()) {
+                liveService.start();
+                certificate = publicKeyCryptoConfig();
+            }
+            Dem.process(() -> {
+                try (final var liveService = net.splitcells.martins.avots.distro.DistroCell.liveService()) {
+                    liveService.start();
+                    Dem.waitIndefinitely();
+                }
+            }, env -> {
+                env.config()
+                        .withConfigValue(PublicIdentityPemStore.class, Optional.of(certificate.publicPem()))
+                        .withConfigValue(PrivateIdentityPemStore.class, Optional.of(certificate.privatePem()))
+                        .withConfigValue(SslEnabled.class, true)
+                        .withInitedOption(HtmlLiveTester.class)
+                        .withConfigValue(HtmlLiveTest.class, TEST_OPTIMIZATION_GUI)
+                        .withConfigValue(MessageFilter.class, logMessage -> true)
+                        /* The default settings sometimes crash the container,
+                         * because of OutOfMemory, although enough memory is present.
+                         * Furthermore, if OutOfMemory does not appear, the browser still do not work.
+                         * This seems to be a Playwright specific error.
+                         *
+                         * TODO Theory: start 1 Playwright instance first and execute its tests.
+                         * After that, one can use as many Playwright instances as needed.
+                         * The reason for that would be, the Playwright's initial setup has a race condition.
+                         *
+                         * TODO Remove this, when multiple live testers work at once on live server. -> This seems to be working.
+                         *
+                         * .withConfigValue(HtmlLiveTesterCount.class, 1)
+                         */
+                        .withConfigValue(InternalPublicPort.class, Optional.of(8443)) // This is required, because from inside the container, the port is not the public one, but the one in the mapping of the Dockerfile.
+                        .withConfigValue(PasswordAuthenticationEnabled.class, true)
+                        .withConfigValue(Authentication.class, authenticatorBasedOnFiles())
+                        .withConfigValue(Authorization.class, authorizerBasedOnFiles())
+                ;
+                baseConfig(env);
+            });
+        }, env -> {
+            final var publicKeyCryptoConfig = selfSignedPublicKeyCryptoConfigurator().selfSignedPublicKeyCryptoConfig();
+            env.config().withConfigValue(PublicIdentityPemStore.class, Optional.of(publicKeyCryptoConfig.publicPem()))
+                    .withConfigValue(PrivateIdentityPemStore.class, Optional.of(publicKeyCryptoConfig.privatePem()))
+                    .withConfigValue(SslEnabled.class, true);
+            baseConfig(env);
+        });
+    }
+
+    private static void baseConfig(Environment env) {
+        setGlobalUnixStateLogger(env);
+        env.config()
+                .withConfigValue(MessageFilter.class, logMessage -> logMessage.priority().greaterThanOrEqual(DEBUG))
+                .withConfigValue(Logs.class, serverLog(env.config().configValue(Console.class)
+                        , env.config().configValue(MessageFilter.class)))
+                .withConfigValue(PublicDomain.class, Optional.of("live.splitcells.net"))
+                .withConfigValue(PublicContactEMailAddress.class, Optional.of("contacts@splitcells.net"))
+                .withConfigValue(AcmeServerUri.class, PRODUCTION_ACME_SERVER)
+                .withInitedOption(RedirectServer.class);
+        DistroCell.configurator(env);
+        net.splitcells.martins.avots.distro.DistroCell.envConfig(env);
+        ensureSslCertificatePresence(env);
+    }
+
+    public static void cellBasedMain(String... args) {
         Dem.serve(LiveDistroCell.class);
     }
 
