88e32b763983128fec18311e57b58cd17dfa24fe
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-14 00:32:15 +0100

    Use podman's compose #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/setup.monitoring.sh
+++ bin/setup.monitoring.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 set -e
-sudo apt-get install -y netavark aardvark-dns
+sudo apt-get install -y podman-compose netavark aardvark-dns
 cd src/main/podman
-podman compose down # Ensure a restart, so that all configs are really applied.
-podman compose up --detach
+podman-compose down # Ensure a restart, so that all configs are really applied.
+podman-compose up --detach
