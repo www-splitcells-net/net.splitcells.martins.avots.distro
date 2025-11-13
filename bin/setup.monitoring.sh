bdf56b16b6ce54ad1979973cc6472e988a20f5b9
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-13 21:48:41 +0100

    Define basic monitoring services #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ bin/setup.monitoring.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+set -e
+sudo apt-get install -y netavark aardvark-dns
+cd src/main/podman
+podman compose down # Ensure a restart, so that all configs are really applied.
+podman compose up --detach
