4a66c4294b45ac28a145948b025a95afd372215a
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-14 20:49:52 +0100

    Doc Grafana connection #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/connect.grafana.sh
+++ bin/connect.grafana.sh
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
 set -e
+echo You can now open Grafana via http://localhost:9000.
 ssh -L 9000:localhost:3000 martins-avots@live.splitcells.net
