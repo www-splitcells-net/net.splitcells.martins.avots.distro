cd4b5b3254670d676f5d3081281caeed1350261c
Mārtiņš Avots <martins.avots@splitcells.net>
2025-11-14 20:46:57 +0100

    Clean up setup.monitoring.sh #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/setup.monitoring.sh
+++ bin/setup.monitoring.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env sh
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
+
+# Add the following, if pulling the repo fails
+# sudo nano /etc/containers/registries.conf
+# unqualified-search-registries = ["docker.io"]
+# Add the following config, if the container cannot connect to each other via their names:
+# sudo nano /etc/containers/containers.conf
+# [network]
+# network_backend = "netavark"
+
 set -e
+
 sudo apt-get install -y podman-compose netavark aardvark-dns
 cd src/main/podman
 podman-compose up --detach
 podman-compose restart # Ensure a restart, so that all configs are really applied, if the pods where already running.
-exit
-
-TODO
-
-admin password
-
-sudo nano /etc/containers/registries.conf
-unqualified-search-registries = ["docker.io"]
-sudo nano /etc/containers/containers.conf
-[network]
-network_backend = "netavark"
