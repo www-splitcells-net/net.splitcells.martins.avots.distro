e63646738c1f951ae0407f83ef6390400de76086
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-08 11:24:15 +0200

    Remove old test commands #8
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- bin/test.live-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
-# SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
-set -e
-[ ! -d "bin" ] && cd ..
-cd ../net.splitcells.network
-bin/worker.test.at martins-avots@live.splitcells.net
