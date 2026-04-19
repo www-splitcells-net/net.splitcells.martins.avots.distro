5c5ab2f60a80a5f4ef5a0a4f995309c40f5630de
Mārtiņš Avots <martins.avots@splitcells.net>
2026-04-19 02:28:36 +0200

    Migrate to SLF4J cell #42
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/martins/avots/distro/LiveDistroCell.java
+++ src/main/java/net/splitcells/martins/avots/distro/LiveDistroCell.java
@@ -1,16 +1,4 @@
-/*
- * Copyright (c) 2021 Contributors To The `net.splitcells.*` Projects
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License, v. 2.0 are satisfied: GNU General Public License v2.0 or later
- * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+/* SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  * SPDX-FileCopyrightText: Contributors To The `net.splitcells.*` Projects
  */
 package net.splitcells.martins.avots.distro;
@@ -24,6 +12,7 @@ import net.splitcells.dem.resource.communication.log.MessageFilter;
 import net.splitcells.dem.resource.profiling.PyroscopeService;
 import net.splitcells.gel.ext.GelExtCell;
 import net.splitcells.network.distro.DistroCell;
+import net.splitcells.network.distro.java.Slf4jCell;
 import net.splitcells.network.distro.java.acme.AcmeServerUri;
 import net.splitcells.network.distro.java.acme.CurrentAcmeAuthorization;
 import net.splitcells.network.system.SystemCell;
@@ -114,9 +103,9 @@ public class LiveDistroCell implements Cell {
 
     @Override
     public void accept(Environment env) {
-        env.withCell(net.splitcells.martins.avots.distro.DistroCell.class);
-        env.config().configValueTyped(net.splitcells.network.distro.java.DistroCell.class)
-                .setGlobalUnixStateLogger();
+        env
+                .withCell(net.splitcells.martins.avots.distro.DistroCell.class)
+                .withCell(Slf4jCell.class);
         GelExtCell.configureForWebserver(env);
         configForPublicServer(env);
     }
